31e24c9b48d4ac1ccb527c0f40f50062b0878b3d
richardhenyash <richardash1@gmail.com>
2021-06-11 08:20:35 +0000

    remove unused imports

--- database.py
+++ database.py
@@ -2,7 +2,4 @@
 from flask_pymongo import PyMongo
 
 # Create an instance of PyMongo, assigned to the varable mongo
-try:
-    mongo = PyMongo()
-except:
-    print("Could not connect to the Mongo DB")
+mongo = PyMongo()
